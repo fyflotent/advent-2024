80ace909c331d43adad3b9ae402729bb3f493785
Chris Rice <chris.189.r@gmail.com>
2025-04-13 12:29:13 -0400

    Finish day 1 a

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,32 @@
+use std::{fs::read_to_string, vec};
+
 fn main() {
-    println!("Hello, world!");
+    let mut val = read_to_string("./src/input.txt")
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let mut it = line.split("   ");
+            let (Some(str1), Some(str2)) = (it.next(), it.next()) else {
+                panic!("Line does not match expectations {}", line)
+            };
+            (str1.parse::<i64>().unwrap(), str2.parse::<i64>().unwrap())
+        })
+        .fold(
+            (vec![0i64; 0], vec![0i64; 0]),
+            |mut vec_tup, (left, right)| {
+                vec_tup.0.push(left);
+                vec_tup.1.push(right);
+                vec_tup
+            },
+        );
+    val.0.sort();
+    val.1.sort();
+    println!(
+        "Result: {:?}",
+        val.0
+            .iter()
+            .zip(val.1.iter())
+            .map(|(left, right)| (right - left).abs())
+            .sum::<i64>()
+    );
 }
