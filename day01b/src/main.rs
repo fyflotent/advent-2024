64004b3d43626a370fc334130d052e4418e0538e
Chris Rice <chris.189.r@gmail.com>
2025-04-13 14:39:11 -0400

    Complete day1b

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,3 +1,33 @@
+use std::{collections::HashMap, fs::read_to_string};
+
 fn main() {
-    println!("Hello, world!");
+    let binding = read_to_string("./src/input.txt").unwrap();
+    let (arr, hash) = binding
+        .lines()
+        .map(|line| match line.split("   ").collect::<Vec<&str>>()[..] {
+            [s1, s2, ..] => (s1.parse::<i64>().unwrap(), s2.parse::<i64>().unwrap()),
+            _ => panic!("Line does not match expectations {}", line),
+        })
+        .fold(
+            (vec![], HashMap::<i64, i64>::new()),
+            |(mut list, mut hash), (left, right)| {
+                list.push(left);
+
+                match hash.get(&right) {
+                    Some(current_count) => hash.insert(right, *current_count + 1),
+                    _ => hash.insert(right, 1),
+                };
+                (list, hash)
+            },
+        );
+
+    println!(
+        "{}",
+        arr.iter()
+            .map(|curr| match hash.get(curr) {
+                Some(count) => count * curr,
+                _ => 0,
+            })
+            .sum::<i64>()
+    );
 }
