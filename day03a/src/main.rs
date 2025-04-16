bb8ca484de38d561a7d9492e2af4de68b9e6f49d
Chris Rice <chris.189.r@gmail.com>
2025-04-15 21:25:46 -0400

    Finish day 3a

--- day03a/src/main.rs
+++ day03a/src/main.rs
@@ -1,3 +1,15 @@
+use regex::Regex;
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let binding = read_to_string("./src/input.txt").unwrap();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut results: Vec<(i64, i64)> = vec![];
+    for (_, [num1, num2]) in re.captures_iter(&binding).map(|c| c.extract()) {
+        results.push((num1.parse::<i64>().unwrap(), num2.parse::<i64>().unwrap()));
+    }
+    println!(
+        "{:?}",
+        results.into_iter().map(|(n1, n2)| n1 * n2).sum::<i64>()
+    );
 }
