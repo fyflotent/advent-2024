2d13df5e77f81e985ff4dcb2a0980f9b0bfa21a9
Chris Rice <chris.189.r@gmail.com>
2025-04-20 18:15:13 -0400

    X-MAS much easier

--- day04b/src/main.rs
+++ day04b/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let all_lines: Vec<Vec<char>> = include_str!("./input.txt")
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+    let mut count = 0;
+    for i in 0..all_lines.len() - 2 {
+        for j in 0..all_lines.len() - 2 {
+            let left_diagonal = String::from_iter([
+                all_lines[i][j],
+                all_lines[i + 1][j + 1],
+                all_lines[i + 2][j + 2],
+            ]);
+            let right_diagonal = String::from_iter([
+                all_lines[i + 2][j],
+                all_lines[i + 1][j + 1],
+                all_lines[i][j + 2],
+            ]);
+            if (left_diagonal == "MAS" || left_diagonal == "SAM")
+                && (right_diagonal == "MAS" || right_diagonal == "SAM")
+            {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{}", count)
 }
